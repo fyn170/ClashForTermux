007fa1edaf25f7d227e3ac0f2c30f0d2b34bf883
Fynn <82778517+fyn170@users.noreply.github.com>
2023-05-22 12:33:44 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,59 +1,61 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
+
 null="> /dev/null 2>&1"
 g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
+
 mkdir -p $HOME/.config/clash
 echo ""
 
-echo -e $b"➤ "$w"Clash For Termux Non Root"
+echo -e "${b}➤ ${w}Clash For Termux Non Root"
 echo ""
 
-echo -e $b"➤ "$w" prepare for installing dependencies ..."
+echo -e "${b}➤ ${w}Preparing for installing dependencies..."
 echo ""
 
-echo -e $b"➤ "$w" installing pacakge: "$g"clash"$w
+echo -e "${b}➤ ${w}Installing package: ${g}clash${w}"
 pkg install clash
 echo ""
 
-echo -e $b"➤ "$w" installing pacakge: "$g"unzip"$w
+echo -e "${b}➤ ${w}Installing package: ${g}unzip${w}"
 pkg install unzip
 echo ""
 
-echo -e $b"➤ "$w" installing pacakge: "$g"micro"$w
+echo -e "${b}➤ ${w}Installing package: ${g}micro${w}"
 pkg install micro
 echo ""
 
 cd $HOME/ClashForTermux
 sleep 2
 
-echo -e $b"➤ "$w" coppying yacd-dashbord: "$g"yacd"$w
+echo -e "${b}➤ ${w}Copying yacd-dashbord: ${g}yacd${w}"
 cp -r -f yacd-gh-pages.zip $HOME/.config/clash
 echo ""
 
 sleep 2
 
-echo -e $b"➤ "$w" coppying vmess.yaml: "$g"vmess"$w
+echo -e "${b}➤ ${w}Copying vmess.yaml: ${g}vmess${w}"
 cp -r -f vmess.yaml $HOME/.config/clash
 echo ""
 
 sleep 2
 
-echo -e $b"➤ "$w" coppying trojan.yaml: "$g"trojan"$w
+echo -e "${b}➤ ${w}Copying trojan.yaml: ${g}trojan${w}"
 cp -r -f trojan.yaml $HOME/.config/clash
 echo ""
 
 sleep 2
 
-echo -e $b"➤ "$w" coppying config.yaml: "$g"config"$w
+echo -e "${b}➤ ${w}Copying config.yaml: ${g}config${w}"
 cp -r -f config.yaml $HOME/.config/clash
 echo ""
 
 sleep 2
 
 cd $HOME/.config/clash
-echo -e $b"➤ "$w" extracting yacd-dashbord: "$g"yacd"$w
+echo -e "${b}➤ ${w}Extracting yacd-dashbord: ${g}yacd${w}"
 unzip yacd-gh-pages.zip
 echo ""
 
